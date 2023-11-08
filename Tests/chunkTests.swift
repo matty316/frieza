dd65c5194594a8f29cd1a055570791ee4d6d68b2
matty <4nem@matty316.com>
2023-11-07 22:02:41 -0600

    remove tests

--- Tests/chunkTests.swift
+++ Tests/chunkTests.swift
@@ -11,29 +11,7 @@ import XCTest
 final class chunkTests: XCTestCase {
 
     func testConstant() throws {
-        var chunk = Chunk()
         
-        var constant = chunk.addConstant(val: 10)
-        chunk.write(byte: OpCode.Constant.rawValue, line: 123)
-        chunk.write(byte: UInt8(constant), line: 123)
-        
-        constant = chunk.addConstant(val: 5)
-        chunk.write(byte: OpCode.Constant.rawValue, line: 123)
-        chunk.write(byte: UInt8(constant), line: 123)
-        
-        constant = chunk.addConstant(val: 5.5)
-        chunk.write(byte: OpCode.Constant.rawValue, line: 123)
-        chunk.write(byte: UInt8(constant), line: 123)
-        
-        chunk.write(byte: OpCode.Return.rawValue, line: 123)
-                
-        var array = chunk.code
-        let url = URL(fileURLWithPath: "/Users/matthewreed/projects/frieza/test")
-        let data = Data(array)
-        try! data.write(to: url)
-        
-        let newData = try! Data(contentsOf: url)
-        print(newData)
     }
 
 }
